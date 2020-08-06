115a72a109d727e58c9dc5dfeb9b7d02fa33bdcc
Magnus Tullberg <magnus.tullberg@debricked.com>
2020-08-06 11:15:11 +0200

    Add default swagger filename to Api, issue 191.
    https://github.com/python-restx/flask-restx/issues/191

--- flask_restx/api.py
+++ flask_restx/api.py
@@ -97,6 +97,7 @@ class Api(object):
     :param url_scheme: If set to a string (e.g. http, https), then the specs_url and base_url will explicitly use this
         scheme regardless of how the application is deployed. This is necessary for some deployments behind a reverse
         proxy.
+    :param str default_swagger_filename: The default swagger filename.
     """
 
     def __init__(
@@ -127,6 +128,7 @@ class Api(object):
         serve_challenge_on_401=False,
         format_checker=None,
         url_scheme=None,
+        default_swagger_filename="swagger.json",
         **kwargs
     ):
         self.version = version
@@ -157,6 +159,7 @@ class Api(object):
         self._refresolver = None
         self.format_checker = format_checker
         self.namespaces = []
+        self.default_swagger_filename = default_swagger_filename
 
         self.ns_paths = dict()
 
@@ -299,7 +302,7 @@ class Api(object):
                 app_or_blueprint,
                 SwaggerView,
                 self.default_namespace,
-                "/swagger.json",
+                "/" + self.default_swagger_filename,
                 endpoint=endpoint,
                 resource_class_args=(self,),
             )
